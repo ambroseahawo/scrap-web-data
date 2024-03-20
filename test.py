26ba3a4ab73db9f4314e60faf07d5d2235a6bd4a
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-03-20 23:55:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-import datetime
 
-current_time = str(datetime.datetime.now())
-
-split_time = current_time.split('.')
-split_date_time = split_time[0].split()
-file_name = '@'.join(split_date_time)
-
-
-print(file_name)
